4a4442cd23ff6401d67a3f9d1c09e10712761e8c
Sergey Solomentsev <Serg.Solomentsev@ya.ru>
2019-08-17 18:10:21 +0300

    Provided first option that allows to specify keys, triggering time showing.

--- showtime.py
+++ showtime.py
@@ -3,25 +3,46 @@ from datetime import datetime
 
 
 class ShowTime(kp.Plugin):
+    """
+    Plugin that is intended to show the current time.
+    """
+
+    # The configuration section that is marked like [time].
+    SETTINGS_TIME_SECTION = "time"
+
+    # The key of the option like show_time_keywords=1,2,3.
+    SETTINGS_TIME_KEYWORDS_OPTION = "show_time_keywords"
+    
+    # Defaults used when getting options of show_time_keywords.
+    TIME_KEYWORDS_DEFAULT = ["time", "now"]
 
     def __init__(self):
+        """
+        Empty constructor, making a call to base.
+        """
         super().__init__()
 
     def on_start(self):
-        self.acceptable_time_inputs = [
-            "time",
-            "now"
-        ]
+
+        options: kp.Settings = self.load_settings()
+
+        self.acceptable_time_inputs = options.get_multiline(
+            self.SETTINGS_TIME_KEYWORDS_OPTION,
+            self.SETTINGS_TIME_SECTION,
+            self.TIME_KEYWORDS_DEFAULT
+        )
 
     def on_suggest(self, user_input, items_chain=[]):
+
         args = {'user_input': user_input}
 
         suggestions = self.get_time_suggestions(args)
-        
+
         self.set_suggestions(list(suggestions),
-                             kp.Match.ANY, kp.Sort.NONE)
+                                kp.Match.ANY, kp.Sort.NONE)
 
     def get_time_suggestions(self, args):
+
         input = args.get("user_input")
 
         text = input.lower()
