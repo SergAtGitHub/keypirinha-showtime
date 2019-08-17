dc777de91f30176a8a85a1e863519dfc5c07b735
Sergey Solomentsev <Serg.Solomentsev@ya.ru>
2019-08-17 18:52:40 +0300

    Added settings which enable configuring title and description.

--- showtime.py
+++ showtime.py
@@ -12,9 +12,15 @@ class ShowTime(kp.Plugin):
 
     # The key of the option like show_time_keywords=1,2,3.
     SETTINGS_TIME_KEYWORDS_OPTION = "show_time_keywords"
-    
+
     # Defaults used when getting options of show_time_keywords.
     TIME_KEYWORDS_DEFAULT = ["time", "now"]
+
+    # The key of the option like show_additional_info_in_description=yes.
+    SETTINGS_TIME_SHOW_DESCRIPTION = "show_additional_info_in_description"
+
+    # Defaults used when getting options of show_additional_info_in_description.
+    DATE_IN_DESCRIPTION_DEFAULT = False
 
     def __init__(self):
         """
@@ -32,6 +38,17 @@ class ShowTime(kp.Plugin):
             self.TIME_KEYWORDS_DEFAULT
         )
 
+        self.show_time_description = options.get_bool(
+            self.SETTINGS_TIME_SHOW_DESCRIPTION,
+            self.SETTINGS_TIME_SECTION,
+            self.DATE_IN_DESCRIPTION_DEFAULT
+        )
+
+        self.time_title_format = options.get(
+            "time_title_format", "time", "%H:%m")
+        self.time_description_format = options.get(
+            "time_description_format", "time", "%x")
+
     def on_suggest(self, user_input, items_chain=[]):
 
         args = {'user_input': user_input}
@@ -39,7 +56,7 @@ class ShowTime(kp.Plugin):
         suggestions = self.get_time_suggestions(args)
 
         self.set_suggestions(list(suggestions),
-                                kp.Match.ANY, kp.Sort.NONE)
+                             kp.Match.ANY, kp.Sort.NONE)
 
     def get_time_suggestions(self, args):
 
@@ -49,12 +66,15 @@ class ShowTime(kp.Plugin):
         if text not in self.acceptable_time_inputs:
             return
 
-        time_label = datetime.now()
+        current_time = datetime.now()
+        time_label = current_time.strftime(self.time_title_format)
+        date_label = self.show_time_description and current_time.strftime(
+            self.time_description_format) or ""
 
         yield self.create_item(
             category=kp.ItemCategory.USER_BASE + 1,
-            label=time_label.strftime("%H:%M"),
-            short_desc="",
+            label=time_label,
+            short_desc=date_label,
 
             # (on_execute) Some value that is passed to on_execute.
             target="target",
